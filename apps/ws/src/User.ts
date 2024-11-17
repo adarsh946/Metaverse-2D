207b3ced77536560afeba420676b10629e65b30a
adarsh946 <adarsh.shukla2724@gmail.com>
2024-11-17 23:08:02 +0530

    added user server in ws

---
+++ apps/ws/src/User.ts
@@ -0,0 +1,59 @@
+import client from "@repo/db/client";
+import { WebSocket } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
+import { RoomManager } from "./RoomManager";
+
+const getStringId = (length: number) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+export class User {
+  public id: string;
+  public userId?: String;
+  private spaceId?: string;
+  private x: number;
+  private y: number;
+  private ws: WebSocket;
+
+  constructor(ws: WebSocket) {
+    this.id = getStringId(10);
+    this.x = 0;
+    this.y = 0;
+    this.ws = ws;
+    this.initHandlers();
+  }
+
+  initHandlers() {
+    this.ws.on("message", async (data) => {
+      const parsedData = JSON.parse(data.toString());
+      console.log(parsedData);
+      switch (parsedData.type) {
+        case "join":
+          const spaceId = parsedData.payload.data;
+          const token = parsedData.payload.token;
+          const userId = (jwt.verify(token, JWT_PASSWORD) as JwtPayload).userId;
+          if (!userId) {
+            this.ws.close();
+            return;
+          }
+          this.userId = userId;
+          const space = await client.space.findFirst({
+            where: {
+              id: spaceId,
+            },
+          });
+          if (!space) {
+            this.ws.close();
+          }
+          this.spaceId = spaceId;
+      }
+    });
+  }
+}
---
+++ apps/ws/src/types.ts
@@ -0,0 +1,1 @@
+export type outGoingMesseges = any;
